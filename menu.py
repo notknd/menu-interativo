e3f3b06f5795f3418d27b3d67c6e2061391d5338
notknd <84822641+notknd@users.noreply.github.com>
2024-11-09 10:14:36 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -9,13 +9,30 @@ def executar_comando(opcao):
         comando = "echo 'Executando opção 2'"
     elif opcao == 2:
         comando = "echo 'Executando opção 3'"
-    else:
-        comando = ""
-    
+    elif opcao == 3:
+        comando = "echo 'Saindo...'"
+
     if platform.system() == "Windows":
         os.system(f'cmd /c {comando}')
     else:
         os.system(comando)
+
+def desenhar_botao(stdscr, y, x, texto, selecionado):
+    largura = 20
+    altura = 5
+
+    if selecionado:
+        stdscr.attron(curses.color_pair(1))
+
+    # Desenha borda do botão
+    for i in range(altura):
+        stdscr.addstr(y + i, x, " " * largura)
+
+    # Adiciona texto centralizado
+    stdscr.addstr(y + altura // 2, x + (largura - len(texto)) // 2, texto)
+
+    if selecionado:
+        stdscr.attroff(curses.color_pair(1))
 
 def mostrar_menu(stdscr):
     curses.curs_set(0)
@@ -24,6 +41,7 @@ def mostrar_menu(stdscr):
 
     menu = ["Opção 1", "Opção 2", "Opção 3", "Sair"]
     opcao_selecionada = 0
+    num_colunas = 2
 
     # Habilita o uso do mouse em curses
     curses.mousemask(1)
@@ -31,25 +49,23 @@ def mostrar_menu(stdscr):
     while True:
         stdscr.clear()
 
-        # Exibe as opções de menu como "botões" em ASCII
+        # Desenha botões em uma grade 2x2
         for idx, opcao in enumerate(menu):
-            x = 5
-            y = 3 + idx * 3
-
-            if idx == opcao_selecionada:
-                stdscr.attron(curses.color_pair(1))
-                stdscr.addstr(y, x, f"[ {opcao} ]")
-                stdscr.attroff(curses.color_pair(1))
-            else:
-                stdscr.addstr(y, x, f"[ {opcao} ]")
+            y = 3 + (idx // num_colunas) * 6
+            x = 5 + (idx % num_colunas) * 25
+            desenhar_botao(stdscr, y, x, opcao, idx == opcao_selecionada)
 
         # Captura a entrada do usuário (teclado ou mouse)
         tecla = stdscr.getch()
 
         # Controle de navegação do teclado
-        if tecla == curses.KEY_UP and opcao_selecionada > 0:
+        if tecla == curses.KEY_UP and opcao_selecionada >= num_colunas:
+            opcao_selecionada -= num_colunas
+        elif tecla == curses.KEY_DOWN and opcao_selecionada < len(menu) - num_colunas:
+            opcao_selecionada += num_colunas
+        elif tecla == curses.KEY_LEFT and opcao_selecionada % num_colunas > 0:
             opcao_selecionada -= 1
-        elif tecla == curses.KEY_DOWN and opcao_selecionada < len(menu) - 1:
+        elif tecla == curses.KEY_RIGHT and opcao_selecionada % num_colunas < num_colunas - 1:
             opcao_selecionada += 1
         elif tecla == curses.KEY_ENTER or tecla in [10, 13]:  # Enter
             if opcao_selecionada == len(menu) - 1:
@@ -62,7 +78,9 @@ def mostrar_menu(stdscr):
             _, mouse_x, mouse_y, _, mouse_state = curses.getmouse()
             if mouse_state & curses.BUTTON1_PRESSED:
                 for idx in range(len(menu)):
-                    if 5 <= mouse_x <= 15 and (3 + idx * 3) == mouse_y:
+                    y = 3 + (idx // num_colunas) * 6
+                    x = 5 + (idx % num_colunas) * 25
+                    if y <= mouse_y < y + 5 and x <= mouse_x < x + 20:
                         opcao_selecionada = idx
                         if opcao_selecionada == len(menu) - 1:
                             return
